5baf2cf4d65cca913c15efd086ffd467f22ac8b6
Okan Çelebi <okancelebi06@gmail.com>
2023-01-12 21:03:24 +0300

    Add files via upload

---
+++ float veri tipi/float veri tipi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace float_veri_tipi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //7 basamaka kadar ondalıklı veri tiplerini tutabilir.
+            //float veri tipi 
+            float ondalikli_sayi1 = 35.56f;
+            float ondalikli_sayi2 = 12.5f;
+            Console.WriteLine(ondalikli_sayi1);
+            Console.WriteLine(ondalikli_sayi2);
+            Console.ReadLine();
+        }
+    }
+}
